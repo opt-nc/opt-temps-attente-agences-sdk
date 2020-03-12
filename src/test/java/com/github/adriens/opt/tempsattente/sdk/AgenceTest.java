70436cf91ac7aa733ab0a7f93266d88f0a690a29
meilie <bossart.emilie@gmail.com>
2020-03-12 22:18:20 +1100

    Récupérer l'ID de l'agence #7 Modifications et Tests pour la récupération de l'ID de l'agence

--- src/test/java/com/github/adriens/opt/tempsattente/sdk/AgenceTest.java
+++ src/test/java/com/github/adriens/opt/tempsattente/sdk/AgenceTest.java
@@ -5,6 +5,10 @@
  */
 package com.github.adriens.opt.tempsattente.sdk;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -20,6 +24,31 @@ public class AgenceTest {
      */
     public AgenceTest() {
     }
+    
+    @Test
+    public void testGetIdAgence() {
+        System.out.println("\ngetIdAgence_TEST");
+
+        Agence instance = new Agence("123456", "designationTEST");
+        
+        String expResult = "123456";
+        String result = instance.getIdAgence();
+
+        assertEquals(expResult, result, "id non trouvé.");
+    }
+    
+    @Test
+    public void testSetIdAgence() {
+        System.out.println("\nsetIdAgence_TEST");
+
+        Agence instance = new Agence();
+
+        String expResult = "123456";
+        instance.setIdAgence("123456");
+        String result = instance.getIdAgence();
+
+        assertEquals(expResult, result, "id non modifié");
+    }
 
     /**
      * Test of getDesignation method, of class Agence.
@@ -28,7 +57,7 @@ public class AgenceTest {
     public void testGetDesignation() {
         System.out.println("\ngetDesignation_TEST");
 
-        Agence instance = new Agence("designationTEST");
+        Agence instance = new Agence("123456", "designationTEST");
 
         String expResult = "designationTEST";
         String result = instance.getDesignation();
@@ -59,11 +88,11 @@ public class AgenceTest {
     public void testGetRealMaxWaitingTime() {
         System.out.println("\ngetRealMaxWaitingTime_TEST");
 
-        Agence instance = new Agence("designationTEST", Agences.ConvertToMillis("00:00:52"));
+        Agence instance = new Agence("123456", "designationTEST", Agences.ConvertToMillis("00:00:52"));
 
         long expResult = Agences.ConvertToMillis("00:00:52");
         long result = instance.getRealMaxWaitingTime();
-        assertEquals(expResult, result, "realMaxWaintingTime non trouvé");
+        assertEquals(expResult, result, "realMaxWaStringingTime non trouvé");
     }
 
     /**
@@ -79,7 +108,26 @@ public class AgenceTest {
         instance.setRealMaxWaitingTime(Agences.ConvertToMillis("00:00:52"));
         long result = instance.getRealMaxWaitingTime();
 
-        assertEquals(expResult, result, "realMaxWaintingTime non modifié");
+        assertEquals(expResult, result, "realMaxWaStringingTime non modifié");
+    }
+    
+    @Test
+    public void testGetAgence() {
+        System.out.println("\ngetAgence_TEST");
+        try {
+            ArrayList<Agence> agences = Agences.getAgences(Agences.Commune.NOUMEA);
+            Agence expResult = new Agence();
+            String idAgence = "4177";
+            for (Agence agence : agences) {
+                if(agence.getIdAgence().equals(idAgence)) expResult = agence;
+            }
+            Agence result = Agence.getAgence(idAgence);
+            assertEquals(expResult.toString(), result.toString(), "Aucune agence trouvée.");
+        } catch (IOException e) {
+            Logger.getLogger(AgencesTest.class.getName()).log(Level.SEVERE, null, e);
+        }
+        
+        
     }
 
     /**
@@ -89,11 +137,12 @@ public class AgenceTest {
     public void testToString() {
         System.out.println("\ntoString_TEST");
 
+        String idAgence = "123456";
         String designation = "designationTEST";
         long realMaxWaitingTime = Agences.ConvertToMillis("00:00:52");
-        Agence instance = new Agence("designationTEST", Agences.ConvertToMillis("00:00:52"));
+        Agence instance = new Agence("123456", "designationTEST", Agences.ConvertToMillis("00:00:52"));
 
-        String expResult = "Agence <designation: " + designation + ", realMaxWaitingTime: " + realMaxWaitingTime + ">";
+        String expResult = "Agence " + idAgence + " <designation: " + designation + ", realMaxWaitingTime: " + realMaxWaitingTime + ">";
         String result = instance.toString();
 
         assertEquals(expResult, result, "Les chaînes sont différentes.");
