7138cdefe342d91619984e992c90d50891e6ddd8
meilie <bossart.emilie@gmail.com>
2020-03-12 22:15:12 +1100

    Récupérer l'ID de l'agence #7 Ajout idAgence + Récupération de l'agence correspondant à idAgence

--- src/main/java/com/github/adriens/opt/tempsattente/sdk/Agence.java
+++ src/main/java/com/github/adriens/opt/tempsattente/sdk/Agence.java
@@ -5,6 +5,10 @@
  */
 package com.github.adriens.opt.tempsattente.sdk;
 
+import static com.github.adriens.opt.tempsattente.sdk.Agences.logger;
+import java.io.IOException;
+import java.util.ArrayList;
+
 /**
  *
  * @author meilie
@@ -16,6 +20,7 @@ package com.github.adriens.opt.tempsattente.sdk;
  */
 public class Agence {
 
+    private String idAgence;
     private String designation;
     private long realMaxWaitingTime;
 
@@ -23,6 +28,7 @@ public class Agence {
      * Default constructor.
      */
     public Agence() {
+        this.idAgence = "";
         this.designation = "";
         this.realMaxWaitingTime = 0;
     }
@@ -33,7 +39,8 @@ public class Agence {
      * @param designation The agency's designation name.
      * @param realMaxWaitingTime the maximum waiting time in the agency.
      */
-    public Agence(String designation, long realMaxWaitingTime) {
+    public Agence(String idAgence, String designation, long realMaxWaitingTime) {
+        this.idAgence = idAgence;
         this.designation = designation;
         this.realMaxWaitingTime = realMaxWaitingTime;
     }
@@ -43,9 +50,14 @@ public class Agence {
      *
      * @param designation The agency's designation name.
      */
-    public Agence(String designation) {
+    public Agence(String idAgence, String designation) {
+        this.idAgence = idAgence;
         this.designation = designation;
         this.realMaxWaitingTime = 0;
+    }
+    
+    public String getIdAgence() {
+        return this.idAgence;
     }
 
     /**
@@ -65,6 +77,10 @@ public class Agence {
     public long getRealMaxWaitingTime() {
         return this.realMaxWaitingTime;
     }
+    
+    public void setIdAgence(String idAgence) {
+        this.idAgence = idAgence;
+    }
 
     /**
      * Set the agency's designation name.
@@ -83,6 +99,24 @@ public class Agence {
     public void setRealMaxWaitingTime(long realMaxWaitingTime) {
         this.realMaxWaitingTime = realMaxWaitingTime;
     }
+    
+    public static Agence getAgence(String idAgence) throws IOException {
+        ArrayList<Agence> agences = Agences.getAgences();
+        Agence result = new Agence();
+        
+        for (Agence agence : agences) {
+            if (agence.idAgence.equals(idAgence)) result = agence;
+        }
+        
+        if (result.idAgence.equals(idAgence)) {
+            logger.info("correspondance trouvée pour " +idAgence+ " : " +result.toString());
+            return result;
+        } else {
+            logger.info("Aucune correspondance pour " +idAgence);
+            return result;
+        }
+        
+    }
 
     /**
      * Return the agency object, as text.
@@ -91,7 +125,7 @@ public class Agence {
      */
     @Override
     public String toString() {
-        return "Agence <designation: " + this.designation + ", realMaxWaitingTime: " + this.realMaxWaitingTime + ">";
+        return "Agence " + this.idAgence + " <designation: " + this.designation + ", realMaxWaitingTime: " + this.realMaxWaitingTime + ">";
     }
 
 }
